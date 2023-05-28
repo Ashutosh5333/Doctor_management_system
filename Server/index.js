7816870d17208c634bcd6c7f1c2bf50be627e843
Ashutosh5333 <lakshakarashutosh@gmail.com>
2023-05-28 16:26:11 +0530

    updatde

---
+++ Server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { connection } = require("./config/db");
+
+
+
+ const app = express()
+ app.use(express.json());
+
+   app.get("/", (req,res) =>{
+        res.send("Welcome home ")
+   })
+
+
+ app.listen(8000,async (req,res) =>{
+       try{
+          await connection
+          console.log("connected to database")
+       }catch(err){
+          console.log("comething went wrong in connected")
+          console.log(err)
+       }
+      console.log("listening on port 8000")
+ })
