7810ec463d64190f50233d891062b8cdccafd9dd
Ashutosh5333 <lakshakarashutosh@gmail.com>
2023-05-29 15:40:46 +0530

    login and signup from done

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,8 +1,132 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {  Text, Box, Image, Heading,} from '@chakra-ui/react'
+import {  Button,  Card,  CardBody,  Input,  InputGroup,  InputLeftElement,  InputRightElement,  VStack,  useColorModeValue,useToast,} from "@chakra-ui/react";
+import {Link} from "react-router-dom"
+import { EmailIcon, UnlockIcon, ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+
+import { useNavigate }from "react-router-dom"
 
 const Login = () => {
+  const colorScheme = useColorModeValue("blue", "green");
+  const [show, setShow] = useState(false);
+
+  const navigate= useNavigate()
+  const toast=useToast()
+
+   const [post ,SetPost]=useState({
+       email:"",
+       password:"",
+   })
+
+    
+    const handleChange = (e) =>{
+        const {name,value}=e.target
+        SetPost({...post,[name]:value})
+    }
+  
+    const handleClickShow = () => {
+      setShow(!show);
+    };
+
+    const handleSubmit = () => {
+        
+    }
+
+
   return (
-    <div>Login</div>
+  <>
+       <Card py="3"  maxW="lg" m="auto" mt="20" >
+          
+          <CardBody  >
+    
+          <Box mt="10" >
+                    <Heading>
+                       Login Form
+                    </Heading>
+                  <Text color={"#FFFFFF"} textAlign={"center"} fontWeight={"600"} > Login </Text>
+           </Box>
+    
+             <Button fontSize={"1.2em"} fontWeight="600" width="full" background={"blue.500"}  colorScheme="blue" color="#fff" mb="10" > Login in with Facebook </Button>
+    
+            <VStack maxW={"2xl"} spacing={5} >
+              <InputGroup position="relative" >
+                <InputLeftElement
+                  pointerEvents="none"
+                  position="absolute"
+                  top="1"
+                  children={<EmailIcon color="gray.400" boxSize={5} />}
+                />
+                <Input
+                  placeholder="Email*"
+                  type="email"
+                  name="email"
+                  size="lg"
+                  onChange={handleChange}
+                />
+              </InputGroup>
+    
+              <InputGroup position="relative">
+                <InputLeftElement
+                  pointerEvents="none"
+                  position="absolute"
+                  top="1"
+                  children={<UnlockIcon color="gray.400" boxSize={5} />}
+                />
+                <Input
+                  type={show ? "text" : "password"}
+                  placeholder="Password*"
+                  name="password"
+                  size="lg"
+                  onChange={handleChange}
+                  
+                />
+                <InputRightElement width="4.5rem" position="absolute" top="1">
+                  <Button
+                    h="1.75rem"
+                    size="lg"
+                    variant="link"
+                    onClick={handleClickShow}
+                  >
+                    {show ? (
+                      <ViewOffIcon color="gray.400" boxSize={5} />
+                    ) : (
+                      <ViewIcon color="gray.400" boxSize={5} />
+                    )}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+    
+              <Button
+                width="100%"
+                size="lg"
+                onClick={handleSubmit}
+                colorScheme={colorScheme}
+                loadingText={"Login"}
+                isDisabled={
+                     post.email=="" || post.password ==""
+                 }
+              >
+                Login
+              </Button>
+    
+            </VStack>
+    
+            <Text m="auto" mt="10" textAlign={"center"}  style={{fontSize:"1em", color:"blue", fontWeight:"600" }}> Forgot password ? 
+    
+               <Link to="/sign">
+              <span style={{fontSize:"1em", color:"black", fontWeight:"500" }}> Create an Account</span> 
+               </Link>
+    
+            </Text>
+    
+          </CardBody>
+    
+    
+          </Card>
+    
+  </>
+
+
   )
 }
 
--- Client/src/pages/Signup.jsx
+++ Client/src/pages/Signup.jsx
@@ -1,8 +1,143 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {  Text, Image,} from '@chakra-ui/react'
+import {  Button,  Card,  CardBody,  Input,  InputGroup,  InputLeftElement,  InputRightElement,  VStack,  useColorModeValue,useToast,} from "@chakra-ui/react";
+import { CiUser } from "react-icons/ci";
+import { EmailIcon, UnlockIcon, ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+
+import { useNavigate }from "react-router-dom"
+
 
 const Signup = () => {
+    const colorScheme = useColorModeValue("blue", "green");
+    const [show, setShow] = useState(false);
+  
+    const navigate= useNavigate()
+    const toast=useToast()
+  
+     const [post ,SetPost]=useState({
+         email:"",
+         password:"",
+         name:""
+     })
+  
+      
+      const handleChange = (e) =>{
+          const {name,value}=e.target
+          SetPost({...post,[name]:value})
+      }
+    
+      const handleClickShow = () => {
+        setShow(!show);
+      };
+  
+      const handleSubmit = () => {
+          
+      }
+  
+
+
+
+
+
+
   return (
-    <div>Signup</div>
+    <Card py="3" maxW="lg" m="auto" mt="20">
+
+    <CardBody>
+    
+
+   
+
+      <Text>  Application Form   </Text>
+
+
+    
+     <Button fontSize={"1.2em"} fontWeight="600" width="full" background={"blue.500"}  colorScheme="blue" color="#fff"
+     mb="5" > Login in with Facebook </Button>
+     
+      <VStack maxW="2xl" spacing={5}>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            position="absolute"
+            top="1"
+            children={<CiUser color="gray" size="23" />}
+          />
+          <Input
+            placeholder="Name*"
+            type="name"
+            name="name"
+            onChange={handleChange}
+            
+            size="lg"
+          />
+        </InputGroup>
+
+
+        <InputGroup position="relative">
+          <InputLeftElement
+            pointerEvents="none"
+            position="absolute"
+            top="1"
+            children={<EmailIcon color="gray.400" boxSize={5} />}
+          />
+          <Input
+            placeholder="Email*"
+            type="email"
+            name="email"
+            size="lg"
+            onChange={handleChange}
+           
+          />
+        </InputGroup>
+
+        <InputGroup position="relative">
+          <InputLeftElement
+            pointerEvents="none"
+            position="absolute"
+            top="1"
+            children={<UnlockIcon color="gray.400" boxSize={5} />}
+          />
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Password*"
+            name="password"
+            size="lg"
+            onChange={handleChange}
+          />
+          <InputRightElement width="4.5rem" position="absolute" top="1">
+            <Button
+              h="1.75rem"
+              size="sm"
+              variant="link"
+              onClick={handleClickShow}
+            >
+              {show ? (
+                <ViewOffIcon color="gray.400" boxSize={5} />
+              ) : (
+                <ViewIcon color="gray.400" boxSize={5} />
+              )}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        <Button
+        onClick={handleSubmit}
+          width="100%"
+          colorScheme={colorScheme}
+          size="lg"
+        >
+          Signup
+        </Button>
+      </VStack>
+
+     <Text  fontSize="1rem"
+       textAlign={"center"} mt={"5"}>
+        . Learn More
+
+     </Text> 
+
+    </CardBody>
+  </Card>
   )
 }
 
