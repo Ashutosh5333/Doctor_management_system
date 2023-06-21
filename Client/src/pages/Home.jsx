2ed75d3d268b02c10e5be65864de9129f02ab889
Ashutosh5333 <lakshakarashutosh@gmail.com>
2023-06-21 11:18:19 +0530

    updated css

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -17,11 +17,19 @@ const Home = () => {
       <Box  width={{base:"70%",md:"50%",lg:"50%"}} h="30vh" m="auto"
       mt={{ base:"20px" ,md:"20px",lg:"30px"}}
       >  
-       <Image  justifyContent={"center"}
+      <Box>
+      <Image  justifyContent={"center"}
       width={{base:"400px",md:"300px",lg:"300px"}}  
        m="auto"
         src={front} />
+      </Box>
 
+      <Box m={"auto"} textAlign={"center"} mb={{ base:"10", md:"20", lg:"40"}}  mt={"10px"}>
+        <Link to="/doctordash">
+        <Button w="300px" bg="#050452" color="#ffffff" p="6"  borderRadius={"20px"}> Home Page  </Button>
+        </Link>
+      </Box>
+      
       </Box>
 
 
@@ -30,7 +38,7 @@ const Home = () => {
        flexDirection={{base:"column",md:"row",lg:"row"}}
        mt={{base:"40%", md:"22%",lg:"160px"}} 
       >
-         <Box   m={{base:"auto"}}  mt={{base:"20px"}}> 
+         <Box   m={{base:"auto"}}  mt={{base:"5"}}> 
           <Link to="/login">
             <Button w="300px" bg="#050452" color="#ffffff" p="6"  borderRadius={"20px"}> Sign in </Button>
           </Link>  
@@ -38,7 +46,7 @@ const Home = () => {
 
         
 
-         <Box  m={{base:"auto"}}     mt={{base:"10px"}}  >
+         <Box  m={{base:"auto"}}     mt={{base:"5"}}  >
           <Link to="/sign"> 
             <Button w="300px" bg="#fff" boxShadow='dark-lg' p='6' borderRadius={"20px"} color="#050452"> Create an Account </Button>
           </Link>
